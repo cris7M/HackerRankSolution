3d3b03e265502e1fb7c397e37051f84a017358f9
cris7M <krishnasanjay010@gmail.com>
2020-10-18 13:19:36 +0530

    5th sol added

---
+++ regex_sol/hack_reg5.py
@@ -0,0 +1,9 @@
+'''
+Matching word and non word charecter
+'''
+
+Regex_Pattern = r"\w\w\w\W\w\w\w\w\w\w\w\w\w\w\W\w\w\w"	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
